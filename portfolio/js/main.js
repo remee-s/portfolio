3a922330702cec28234753193eab6587e09fce72
remee-s <rudfla5927@naver.com>
2025-07-24 11:38:40 +0900

    .

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -5,4 +5,13 @@ $(document).ready(function(){
 		duration: 500, // 애니메이션 효과가 작동되는 시간
 		easing: 'ease', // 가속도
 	});
+
+	//마우스 커서 모양 바꾸는 js
+	$(window).on('scroll mousemove', function(e){  /* html cursor가 마우스 포인터를 따라다니게 하는 값 */
+		$('.cursor').css('left', e.pageX + 'px');
+		$('.cursor').css('top', e.pageY + 'px');
+	});
+	$('.popup').hover(function(){ /* 특정한 요소에 마우스를 올렸을때만 on 클래스 주기 */
+		$('.cursor').toggleClass('on');
+	});
 })
