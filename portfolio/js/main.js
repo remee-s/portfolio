4b154d201792e657441471e146ff141837952475
remee-s <rudfla5927@naver.com>
2025-07-24 12:19:13 +0900

    .

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -8,10 +8,10 @@ $(document).ready(function(){
 
 	//마우스 커서 모양 바꾸는 js
 	$(window).on('scroll mousemove', function(e){  /* html cursor가 마우스 포인터를 따라다니게 하는 값 */
-		$('.cursor').css('left', e.pageX + 'px');
-		$('.cursor').css('top', e.pageY + 'px');
+		$('.contents .txt .cursor').css('left', e.pageX + 'px');
+		$('.contents .txt .cursor').css('top', e.pageY + 'px');
 	});
-	$('.popup').hover(function(){ /* 특정한 요소에 마우스를 올렸을때만 on 클래스 주기 */
-		$('.cursor').toggleClass('on');
+	$('.contents .txt ul li a').hover(function(){ /* 특정한 요소에 마우스를 올렸을때만 on 클래스 주기 */
+		$('.contents .txt .cursor').toggleClass('on');
 	});
 })
